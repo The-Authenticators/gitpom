b7cf275117cbe734a2ed2467042552e2539765e7
John Whiles <johnwhiles@outlook.com>
2016-12-07 14:59:12 +0000

    add a test for login route

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,6 +1,6 @@
 const server = require('../src/server.js');
 const tape = require('tape');
-
+const url = require('url');
 tape('Test that the server starts', (t) => {
   server.start((err) => {
     if (err) {
@@ -9,5 +9,20 @@ tape('Test that the server starts', (t) => {
       t.ok('The server should start');
     }
     server.stop(() => { t.end(); });
+  });
+});
+
+tape('login redirects to github', (t) => {
+  const options = {
+    method: 'GET',
+    url: '/login'
+  };
+  server.inject(options, (res) => {
+    t.equal(res.statusCode, 302, 'status code should be redirect, 302');
+    const actual = url.parse(res.headers.location);
+    t.equal(`${actual.protocol}${actual.host}${actual.pathname}`,
+      `https:github.com/login/oauth/authorize`,
+      `redirect url should match expected`);
+    t.end();
   });
 });
