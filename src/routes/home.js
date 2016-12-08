280803a7690ab14aa1f9868a495d9936edd72d9e
marisid <sid_marina@hotmail.com>
2016-12-08 12:39:58 +0000

    Update repos partial

--- src/routes/home.js
+++ src/routes/home.js
@@ -17,10 +17,11 @@ module.exports = {
 
     if (req.auth.isAuthenticated) {
       // add cookie info here!
-      getRepos(req.auth.credentials.userName, (err, res, body) => {
+      getRepos(req.auth.credentials, (err, res, body) => {
         if (err) { throw err; }
         const currentUser = { userName: req.auth.credentials.userName };
         const repos = { repos: JSON.parse(body) };
+        console.log(repos);
         rep.view('home', Object.assign(homeObj, repos, currentUser));
       });
     } else {
