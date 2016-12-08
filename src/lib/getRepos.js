d7cf8571e8367585b7923387c28191296c88cefe
marisid <sid_marina@hotmail.com>
2016-12-08 11:05:38 +0000

    Get all user repos

--- src/lib/getRepos.js
+++ src/lib/getRepos.js
@@ -2,9 +2,11 @@ const Request = require('request');
 
 module.exports = (user, cb) => {
   Request.get({
-    url: `https://api.github.com/users/${user}/repos`,
+    url: `https://api.github.com/user/repos`,
     headers: {
-      'User-Agent': 'GitPom'
+      'User-Agent': 'GitPom',
+      Accept: `application/vnd.github.v3+json`,
+      Authorization: `token ${user.access_token}`
     }
   }, cb);
 };
